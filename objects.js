83f4eb83ffb1bd7a3fa848899332440bf78cfb6d
rudakov17 <110357201+rudakov17@users.noreply.github.com>
2022-12-22 15:35:35 +0300

    objects HW

--- objects.js
+++ objects.js
@@ -1,4 +1,9 @@
-// создал, добавил, удалил. и метод ещё есть.
+// создал, добавил, удалил. и метод ещё есть. и прототип.
+const objZero = {
+  country: 'Romania',
+  source: 'my mind',
+};
+
 const obj1 = {};
 
 obj1.name = 'Vlad';
@@ -8,6 +13,39 @@ obj1.dateOfDeath = 1479;
 obj1.howOld = function () {
   return 2022 - obj1.dateOfBirth;
 };
+
+obj1.__proto__ = objZero;
+
 delete obj1.dateOfDeath;
 obj1.age = obj1.howOld();
 console.log(obj1);
+
+// задание 1
+function getAllKeys(obj) {
+  for (key in obj)
+    if (obj.hasOwnProperty(key)) {
+      console.log(key);
+    }
+}
+
+getAllKeys(obj1);
+
+// задание 2
+function hasThisKey(str, obj) {
+  for (key in obj) {
+    if (str === key) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(hasThisKey('age', obj1));
+console.log(hasThisKey('allo', obj1));
+
+//задание 3
+const emptyObj = Object.create(null);
+console.log(emptyObj);
+
+const emptyObj2 = {};
+console.log(emptyObj2);
